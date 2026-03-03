8ea5d5ea166921b9b6fb994e324867bf2f49cf60
Sabar <sabar@local.dev>
2026-03-04 05:53:16 +0700

    Fix 500 error by adding missing Carbon import in ClientPageController

--- app/Http/Controllers/Web/ClientPageController.php
+++ app/Http/Controllers/Web/ClientPageController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 use Throwable;
